849ad2df210832066a16972a90cf6baa1be9d44a
rhamza783 <70859024+rhamza783@users.noreply.github.com>
2024-04-23 17:00:55 +0500

    Create rest.sh

---
+++ rest.sh
@@ -0,0 +1,26 @@
+#!bin/bash
+#Coded by 7ife
+clear
+echo ""
+echo -e "\033[1;92m ‎"
+echo "${k}█▄─█▀▀▀█─▄█▄─▄▄─█▄─▄███─▄▄▄─█─▄▄─█▄─▀█▀─▄█▄─▄▄─█";
+echo "${k}██─█─█─█─███─▄█▀██─██▀█─███▀█─██─██─█▄█─███─▄█▀█";
+echo "${k}▀▀▄▄▄▀▄▄▄▀▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▀▄▄▄▀▄▄▄▀▄▄▄▄▄▀";
+echo ""
+echo "${k}█▄─▄─▀██▀▄─██─▄▄▄─█▄─█─▄█";
+echo "${k}██─▄─▀██─▀─██─███▀██─▄▀██";
+echo "${k}▀▄▄▄▄▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▀▄▄▀";
+echo ""
+echo -e "\033[1;90m  Github: github.com/7ife"
+echo -e "\033[1;90m  E-mail: 7ife@pm.me"
+echo ""
+echo -e $'\e[1;34m::TGBA::\e[0m\e[1;32m #Bot launch\e[0m'
+sleep 2
+echo -e " \033[1;93m Start Bot"
+echo ""
+npm start
+echo ""
+while true
+do
+sleep 5
+done
