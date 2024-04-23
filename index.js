b9aae139bfa55cfcefcee4cba8bc3c7a346b54a8
rhamza783 <70859024+rhamza783@users.noreply.github.com>
2024-04-23 16:00:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,101 +1,165 @@
 // Connecting modules
+
 const Telegraf = require('telegraf');
-const fs = require('fs'); // File system module for logging
+
+// const HttpsProxyAgent = require('https-proxy-agent');
 
 // General settings
+
 let config = {
+
     "token": "YOUR_TOKEN", // Bot token
-    "admin": 123456789 // bot owner id (replace with actual ID)
+
+    "admin": 034567888 // bot owner id
+
 };
-const CHAT_ID = 123456789; // bot owner id (replace with actual ID)
+
+const CHAT_ID = 034567888 ; // bot owner id
 
 // Creating a bot object
-const bot = new Telegraf(config.token);
 
-// Function to create a hidden link
-function getHiddenLink(url, parse_mode = "markdown") {
-    const emptyChar = "‎";
-    switch (parse_mode) {
-        case "HTML":
-            return `<a href="${url}">${emptyChar}</a>`;
-        default:
-            throw new Error("invalid parse_mode");
+const bot = new Telegraf(config.token, {
+
+        // If you need to go through a proxy, specify it: user, pass, host, port
+
+        // telegram: { agent: new HttpsProxyAgent('http://user:pass@host:port') }
+
     }
-}
 
-// Message to confirm successful installation
-bot.telegram.sendMessage(
-    CHAT_ID,
-    `
-    <b>Great, you have successfully installed a feedback bot!</b>
-    ${getHiddenLink("https://your-link.com/image.png", "HTML")}
-    `,
-    { parse_mode: "HTML" }
 );
 
+
+
+function getHiddenLink(url, parse_mode = "markdown") {
+
+  const emptyChar = "‎";
+
+
+
+  switch (parse_mode) {
+
+    case "HTML":
+
+      return `<a href="${url}">${emptyChar}</a>`;
+
+    default:
+
+      throw new Error("invalid parse_mode");
+
+  }
+
+}
+
+
+
+// message to successfully installed
+
+bot.telegram.sendMessage(
+
+  CHAT_ID,
+
+  `
+
+<b>Great, you have successfully installed a feedback bot!</b>
+
+${getHiddenLink("https://raw.githubusercontent.com/7ife/7ife.github.io/master/data/tgba-logo.png", "HTML")}
+
+`,
+
+  {
+
+    parse_mode: "HTML",
+
+  }
+
+);
+
+
+
 // Text Settings
+
 let replyText = {
+
     "helloAdmin": "Now share your bot and wait for messages.",
-    "helloUser": "Greetings, send me a message. I will try to answer as soon as possible.",
-    "replyWrong": "Please use the Reply function to reply to the user's message directly.",
-    "help": "Here are the commands you can use: /feedback, /report, /help",
-    "feedback": "Thank you for your feedback!",
-    "report": "Your report has been recorded. We will look into it shortly."
+
+    "helloUser":  "Greetings, send me a message. I will try to answer as soon as possible.",
+
+    "replyWrong": "Use the Reply function to reply to the user."
+
 };
 
 // Checking the user's rights
+
 let isAdmin = (userId) => {
+
     return userId == config.admin;
+
 };
 
-// Function to log messages
-function logMessage(message) {
-    fs.appendFile('bot.log', message + '\n', (err) => {
-        if (err) throw err;
-    });
-}
-
 // We redirect the admin from the user or notify the admin about the error
+
 let forwardToAdmin = (ctx) => {
+
     if (isAdmin(ctx.message.from.id)) {
+
         ctx.reply(replyText.replyWrong);
+
     } else {
+
         ctx.forwardMessage(config.admin, ctx.from.id, ctx.message.id);
-        logMessage(`Message from ${ctx.from.id}: ${ctx.message.text}`);
+
     }
+
 };
 
 // Bot Start
-bot.start((ctx) => {
-    ctx.reply(isAdmin(ctx.message.from.id) ? replyText.helloAdmin : replyText.helloUser);
-    logMessage(`Start command used by ${ctx.from.id}`);
-});
 
-// Command handlers
-bot.command('help', (ctx) => ctx.reply(replyText.help));
-bot.command('feedback', (ctx) => {
-    ctx.reply(replyText.feedback);
-    logMessage(`Feedback from ${ctx.from.id}: ${ctx.message.text}`);
-});
-bot.command('report', (ctx) => {
-    ctx.reply(replyText.report);
-    logMessage(`Report from ${ctx.from.id}: ${ctx.message.text}`);
+bot.start((ctx) => {
+
+    ctx.reply(isAdmin(ctx.message.from.id)
+
+        ? replyText.helloAdmin
+
+        : replyText.helloUser);
+
 });
 
 // Listening for the presence of the message object
+
 bot.on('message', (ctx) => {
-    if (ctx.message.reply_to_message && ctx.message.reply_to_message.forward_from && isAdmin(ctx.message.from.id)) {
+
+    // make sure it was the admin who responded to the user's message
+
+    if (ctx.message.reply_to_message
+
+        && ctx.message.reply_to_message.forward_from
+
+        && isAdmin(ctx.message.from.id)) {
+
+        // we send a copy to the user
+
         ctx.telegram.sendCopy(ctx.message.reply_to_message.forward_from.id, ctx.message);
+
     } else {
+
+        // redirecting to the admin
+
         forwardToAdmin(ctx);
+
     }
+
 });
 
 // bot launch
+
 bot.launch()
+
     .then(() => console.log("Bot Launched"))
-    .catch(console.error);
+
+    .catch(console.log);
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+
+process.once('SIGTERM', () => bot.stop('SIGTERM')) 
